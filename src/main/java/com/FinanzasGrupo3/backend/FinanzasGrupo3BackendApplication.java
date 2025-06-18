a1b87be1f4499050e1c58011032effb5bc50924c
sxaxcxs <102533732+sxaxcxs@users.noreply.github.com>
2025-06-18 00:31:16 -0500

    Subida alpha del backend. (pendiente corregir YAML y errores de build)

---
+++ src/main/java/com/FinanzasGrupo3/backend/FinanzasGrupo3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.FinanzasGrupo3.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasGrupo3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasGrupo3BackendApplication.class, args);
+	}
+
+}
